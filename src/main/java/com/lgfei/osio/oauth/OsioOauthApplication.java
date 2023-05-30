31cc17f218583bcd04806c6a8ba859d1e07e3e1a
long.gf <long.gf@topsports.com.cn>
2023-05-30 12:16:23 +0800

    init by https://start.aliyun.com/

---
+++ src/main/java/com/lgfei/osio/oauth/OsioOauthApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsioOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsioOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgfei/osio/oauth/OsioOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsioOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
